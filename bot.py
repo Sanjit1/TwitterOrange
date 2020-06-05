e778eb74bfea201ab0218658374ce8e619fb17d9
Sanjit Sarda <sanjitsarda4@gmail.com>
2020-06-04 22:52:34 -0700

    fixed stuff

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import time
 import tweepy
 import KEYS_AND_TOKENS as k
+import random
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(k.CONSUMER_KEY, k.CONSUMER_SECRET)
@@ -11,9 +12,9 @@ api = tweepy.API(auth)
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
-    counter = 0
+counter = 0
 while True:
-    counter++
-    api.update_with_media(("download (" + randint(1, 26) + ").jfif"), status=("@realDonaldTrump this is your hourly reminder that you are Orange. #" + counter))
+    counter+=1
+    api.update_with_media(("./images/download (" + str(random.randint(1, 26)) + ").jfif"), status=("@realDonaldTrump this is your hourly reminder that you are Orange. #" + str(counter)))
     print(counter)
-    time.sleep(3600)
+    time.sleep(36)
